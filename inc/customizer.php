cfc9f161f5edbaa8ebcb5d624e0345c92f897f1a
Ajwad Rauf <raufa@sheridancollege.ca>
2016-03-20 16:10:56 -0400

    Test
    Test

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Underscore Pets Theme Customizer.
+ *
+ * @package Underscore_Pets
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function underscore_pets_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'underscore_pets_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function underscore_pets_customize_preview_js() {
+	wp_enqueue_script( 'underscore_pets_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'underscore_pets_customize_preview_js' );
